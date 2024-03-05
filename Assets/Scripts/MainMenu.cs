084efe19d040db01a1c27565f85086411910c04f
Bleedem <jjacobwow@gmail.com>
2024-03-05 18:57:12 -0500

    Huge Asset Update

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("Intro");
     }
 
     public void Rules()
